f346b89c413b047fa0fd806013999a7b53d4a8e9
MegaCoom <92753091+MegaCoom@users.noreply.github.com>
2021-10-18 20:48:33 +0000

    Removed ss.alefvanoon.xyz

--- src/assets/javascripts/helpers/google-search.js
+++ src/assets/javascripts/helpers/google-search.js
@@ -2,7 +2,6 @@ const targets = /https?:\/\/(((www|maps)\.)?(google\.).*(\/search)|search\.(goog
 const redirects = [
   { link: "https://sx.alefvanoon.xyz", q: "/search" },
   { link: "https://s.alefvanoon.xyz", q: "/search" },
-  { link: "https://ss.alefvanoon.xyz", q: "/search" },
   { link: "https://search.disroot.org", q: "/search" },
   { link: "https://searx.tuxcloud.net", q: "/search" },
   { link: "https://search.zdechov.net", q: "/search" }
