43376746a6f99ec26efe2215139282ac2a75a61c
MegaCoom <megacoom@cock.lt>
2021-11-07 21:49:36 +0100

    removed dead search engine

--- src/assets/javascripts/helpers/google-search.js
+++ src/assets/javascripts/helpers/google-search.js
@@ -1,7 +1,6 @@
 const targets = /https?:\/\/(((www|maps)\.)?(google\.).*(\/search)|search\.(google\.).*)/;
 const redirects = [
   { link: "https://sx.alefvanoon.xyz", q: "/search" },
-  { link: "https://s.alefvanoon.xyz", q: "/search" },
   { link: "https://search.disroot.org", q: "/search" },
   { link: "https://searx.tuxcloud.net", q: "/search" },
   { link: "https://search.zdechov.net", q: "/search" }
